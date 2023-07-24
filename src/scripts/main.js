046ad186cf1a0702da4c7b795fca69aeeaffd05c
Luke Harby <Luke.Harby@akqa.com>
2023-07-24 10:02:00 +0100

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,29 +1,29 @@
-import FauxCode from './FauxCode';
-import gists from './gists.json';
+import FauxCode from "./FauxCode";
+import gists from "./gists.json";
 
 const options = {
-  theme: 'light', // 'light' or 'dark' mode
+  theme: "light", // 'light' or 'dark' mode
   fontSize: 5, // Line thickness and width
   leading: 10, // Space between lines
-  lineCap: 'round', // Line ends 'square' or 'round'
+  lineCap: "round", // Line ends 'square' or 'round'
   margin: 50, // Space between canvas edges and code block
   lineNumbers: true, // Whether or not to include line numbers
   lineNumberOffset: -3, // Line number offset from margin
 };
 
 const getSettings = () => {
-  const url = document.querySelector('#url').value;
+  const url = document.querySelector("#url").value;
   // get desired theme
-  const light = document.querySelector('#light');
-  const theme = light.checked ? 'light' : 'dark';
+  const light = document.querySelector("#light");
+  const theme = light.checked ? "light" : "dark";
   // Get desired line end style
-  const rounded = document.querySelector('#rounded');
-  const lineCap = rounded.checked ? 'round' : 'square';
+  const rounded = document.querySelector("#rounded");
+  const lineCap = rounded.checked ? "round" : "square";
   return { url, theme, lineCap };
 };
 
-const downloadLink = document.querySelector('.output-download');
-const downloadButton = document.querySelector('.output-button');
+const downloadLink = document.querySelector(".output-download");
+const downloadButton = document.querySelector(".output-button");
 const makeDownloadable = (svgString) => {
   const href = `data:application/octet-stream;base64,${btoa(svgString)}`;
   downloadLink.href = href;
@@ -31,7 +31,7 @@ const makeDownloadable = (svgString) => {
 };
 
 const createSVG = (svgString) => {
-  const mimeType = 'image/svg+xml';
+  const mimeType = "image/svg+xml";
   const doc = new DOMParser().parseFromString(svgString, mimeType);
   const svg = doc.documentElement;
   return svg;
@@ -39,15 +39,15 @@ const createSVG = (svgString) => {
 
 const addElementTo = (element, parentId) => {
   const parent = document.getElementById(parentId);
-  parent.innerHTML = '';
+  parent.innerHTML = "";
   parent.appendChild(parent.ownerDocument.importNode(element, true));
 };
 
 const extractCodeBlock = (body) => {
-  const mimeType = 'text/html';
+  const mimeType = "text/html";
   const doc = new DOMParser().parseFromString(body, mimeType);
-  const desktop = doc.querySelectorAll('.blob-code-inner'); // desktop version
-  const mobile = doc.querySelectorAll('.line'); // mobile version
+  const desktop = doc.querySelectorAll(".blob-code-inner"); // desktop version
+  const mobile = doc.querySelectorAll(".line"); // mobile version
   const codeBlock = [...desktop, ...mobile];
   return codeBlock;
 };
@@ -57,31 +57,31 @@ const start = (url, settings) => {
     .then((response) => response.text())
     .then((body) => extractCodeBlock(body))
     .then((codeBlock) => {
-      const fauxCode = (new FauxCode(codeBlock, settings)).render();
+      const fauxCode = new FauxCode(codeBlock, settings).render();
       const svg = createSVG(fauxCode);
-      addElementTo(svg, 'fauxcode');
+      addElementTo(svg, "fauxcode");
       makeDownloadable(fauxCode);
     })
     .catch((error) => {
-      console.error('Caught error: ', error);
+      console.error("Caught error: ", error);
     });
 };
 
 const startButtonHandler = () => {
   const settings = getSettings();
-  if (!!settings.url && settings.url.includes('gist.github.com')) {
+  if (!!settings.url && settings.url.includes("gist.github.com")) {
     const { theme, lineCap, url } = settings;
     options.theme = theme;
     options.lineCap = lineCap;
     start(url, options);
 
-    const outputSection = document.querySelector('section.output');
-    outputSection.classList.add('ready');
+    const outputSection = document.querySelector("section.output");
+    outputSection.classList.add("ready");
   }
 };
 
-const startButton = document.querySelector('#start');
-startButton.addEventListener('click', startButtonHandler);
+const startButton = document.querySelector("#start");
+startButton.addEventListener("click", startButtonHandler);
 
 const randomFromList = (list) => {
   const index = Math.floor(Math.random() * list.length);
@@ -91,9 +91,22 @@ const randomFromList = (list) => {
 
 const getNewUrl = () => {
   const nextUrl = randomFromList(gists);
-  const url = document.getElementById('url');
+  const url = document.getElementById("url");
   url.value = nextUrl;
 };
 
-const renew = document.getElementById('renew');
-renew.addEventListener('click', getNewUrl);
+const renew = document.getElementById("renew");
+renew.addEventListener("click", getNewUrl);
+
+(() => {
+  const includes = document.getElementsByTagName("include");
+  [].forEach.call(includes, (i) => {
+    let filePath = i.getAttribute("src");
+    fetch(filePath).then((file) => {
+      file.text().then((content) => {
+        i.insertAdjacentHTML("afterend", content);
+        i.remove();
+      });
+    });
+  });
+})();
